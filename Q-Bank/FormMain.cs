ad0bf92e54627db4ac6d3104026ebc2413da7294
SanderBenjamins <sjanne1992@hotmail.com>
2014-12-01 13:08:23 +0100

    1e commit

---
+++ Q-Bank/FormMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.EntityClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q_Bank
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var con = new EntityConnection("name=Q_BANKEntities"))
+            {
+                con.Open();
+                EntityCommand cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT VALUE us FROM Q_BANKEntities.Users as us";
+                Dictionary<int, int> dict = new Dictionary<int, int>();
+                using (EntityDataReader rdr = cmd.ExecuteReader(CommandBehavior.SequentialAccess | CommandBehavior.CloseConnection))
+                {
+                    while (rdr.Read())
+                    {
+                        int a = rdr.GetInt32(0);
+                        var b = rdr.GetInt32(1);
+                        dict.Add(a, b);
+                    }
+                }
+            }
+        }
+    }
+}

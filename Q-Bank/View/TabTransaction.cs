d147a36aa3c74641d88f6e0ed9535880be03552e
SanderBenjamins <sjanne1992@hotmail.com>
2014-12-01 15:06:41 +0100

    Sander is baas, Transaction doet het.

--- Q-Bank/View/TabTransaction.cs
+++ Q-Bank/View/TabTransaction.cs
@@ -10,10 +10,38 @@ namespace Q_Bank.View
 {
     public class TabTransaction
     {
-        public Form FormMain { get; set; }
-        public TabTransaction(Form FormMain)
+        public FormMain formMain { get; set; }
+        public TabTransaction(FormMain formMain)
         {
-            this.FormMain = FormMain;
+            this.formMain = formMain;
+            formMain.transactionButton1.Click += random;
+        }
+
+        public void random(object sender, System.EventArgs e) {
+
+            using (var con = new Q_BANKEntities())
+            {
+        
+                transaction newTransaction = new transaction() { accountId = 2, transactionTypeId = 1, transactionStatusId = 1,
+                                                                 amount = Convert.ToDouble(formMain.transactionNumericUpDown1.Text),
+                                                                 datetime = DateTime.Today,
+                                                                 commit = 0,
+                                                                 executeDatetime = Convert.ToDateTime(formMain.transactionDateTimePicker1.Value),
+                                                                 nameReceiver = formMain.transactionTextBox2.Text,
+                                                                 ibanReceiver = "lalalalala" ,
+                                                                 remark = formMain.transactionTextBox3.Text
+                };
+               
+
+                con.transactions.Add(newTransaction);
+                con.SaveChanges();
+            }
+
+            formMain.transactionTextBox1.Text = String.Empty;
+            formMain.transactionTextBox2.Text = String.Empty;
+            formMain.transactionTextBox3.Text = String.Empty;
+            formMain.transactionDateTimePicker1.Text = DateTime.Today.ToString();
+            formMain.transactionNumericUpDown1.Text = "0.00";
         }
     }
 }

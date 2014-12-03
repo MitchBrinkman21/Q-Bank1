fe6fdb2628cb56df97ce1804955c63659d90b473
Joey Klaassen <joeyklaassen@gmail.com>
2014-12-03 15:29:27 +0100

    - Updated CreateUser.Designer.cs - Added CreateUserController.cs

--- Q-Bank/View/CreateUser.Designer.cs
+++ Q-Bank/View/CreateUser.Designer.cs
@@ -28,11 +28,377 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelAccountAanmaken = new System.Windows.Forms.Label();
+            this.labelBSN = new System.Windows.Forms.Label();
+            this.labelVoornaam = new System.Windows.Forms.Label();
+            this.labelAchternaam = new System.Windows.Forms.Label();
+            this.labelGeslacht = new System.Windows.Forms.Label();
+            this.labelAdres = new System.Windows.Forms.Label();
+            this.labelHuisnummer = new System.Windows.Forms.Label();
+            this.labelPostcode = new System.Windows.Forms.Label();
+            this.labelStad = new System.Windows.Forms.Label();
+            this.labelLand = new System.Windows.Forms.Label();
+            this.labelTelefoonnummer = new System.Windows.Forms.Label();
+            this.labelTelefoonnummerType = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxTelType = new System.Windows.Forms.TextBox();
+            this.textBoxTel = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxBSN = new System.Windows.Forms.TextBox();
+            this.textBoxVoornaam = new System.Windows.Forms.TextBox();
+            this.textBoxAchternaam = new System.Windows.Forms.TextBox();
+            this.comboBoxGeslacht = new System.Windows.Forms.ComboBox();
+            this.textBoxAdres = new System.Windows.Forms.TextBox();
+            this.textBoxHuisnummer = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxStad = new System.Windows.Forms.TextBox();
+            this.textBoxLand = new System.Windows.Forms.TextBox();
+            this.labelGebruikersnaam = new System.Windows.Forms.Label();
+            this.labelWachtwoord = new System.Windows.Forms.Label();
+            this.labelHerhaalWachtwoord = new System.Windows.Forms.Label();
+            this.textBoxGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.textBoxWachtwoord = new System.Windows.Forms.TextBox();
+            this.textBoxHerhaalWachtwoord = new System.Windows.Forms.TextBox();
+            this.buttonBevestigen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelAccountAanmaken
+            // 
+            this.labelAccountAanmaken.AutoSize = true;
+            this.labelAccountAanmaken.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAccountAanmaken.Location = new System.Drawing.Point(94, 29);
+            this.labelAccountAanmaken.Name = "labelAccountAanmaken";
+            this.labelAccountAanmaken.Size = new System.Drawing.Size(215, 29);
+            this.labelAccountAanmaken.TabIndex = 0;
+            this.labelAccountAanmaken.Text = "Account aanmaken";
+            // 
+            // labelBSN
+            // 
+            this.labelBSN.AutoSize = true;
+            this.labelBSN.Location = new System.Drawing.Point(96, 103);
+            this.labelBSN.Name = "labelBSN";
+            this.labelBSN.Size = new System.Drawing.Size(53, 17);
+            this.labelBSN.TabIndex = 1;
+            this.labelBSN.Text = "BSN/NI";
+            // 
+            // labelVoornaam
+            // 
+            this.labelVoornaam.AutoSize = true;
+            this.labelVoornaam.Location = new System.Drawing.Point(96, 136);
+            this.labelVoornaam.Name = "labelVoornaam";
+            this.labelVoornaam.Size = new System.Drawing.Size(73, 17);
+            this.labelVoornaam.TabIndex = 2;
+            this.labelVoornaam.Text = "Voornaam";
+            // 
+            // labelAchternaam
+            // 
+            this.labelAchternaam.AutoSize = true;
+            this.labelAchternaam.Location = new System.Drawing.Point(96, 169);
+            this.labelAchternaam.Name = "labelAchternaam";
+            this.labelAchternaam.Size = new System.Drawing.Size(84, 17);
+            this.labelAchternaam.TabIndex = 3;
+            this.labelAchternaam.Text = "Achternaam";
+            // 
+            // labelGeslacht
+            // 
+            this.labelGeslacht.AutoSize = true;
+            this.labelGeslacht.Location = new System.Drawing.Point(96, 202);
+            this.labelGeslacht.Name = "labelGeslacht";
+            this.labelGeslacht.Size = new System.Drawing.Size(64, 17);
+            this.labelGeslacht.TabIndex = 4;
+            this.labelGeslacht.Text = "Geslacht";
+            // 
+            // labelAdres
+            // 
+            this.labelAdres.AutoSize = true;
+            this.labelAdres.Location = new System.Drawing.Point(96, 235);
+            this.labelAdres.Name = "labelAdres";
+            this.labelAdres.Size = new System.Drawing.Size(45, 17);
+            this.labelAdres.TabIndex = 5;
+            this.labelAdres.Text = "Adres";
+            // 
+            // labelHuisnummer
+            // 
+            this.labelHuisnummer.AutoSize = true;
+            this.labelHuisnummer.Location = new System.Drawing.Point(96, 268);
+            this.labelHuisnummer.Name = "labelHuisnummer";
+            this.labelHuisnummer.Size = new System.Drawing.Size(87, 17);
+            this.labelHuisnummer.TabIndex = 6;
+            this.labelHuisnummer.Text = "Huisnummer";
+            // 
+            // labelPostcode
+            // 
+            this.labelPostcode.AutoSize = true;
+            this.labelPostcode.Location = new System.Drawing.Point(96, 301);
+            this.labelPostcode.Name = "labelPostcode";
+            this.labelPostcode.Size = new System.Drawing.Size(67, 17);
+            this.labelPostcode.TabIndex = 7;
+            this.labelPostcode.Text = "Postcode";
+            // 
+            // labelStad
+            // 
+            this.labelStad.AutoSize = true;
+            this.labelStad.Location = new System.Drawing.Point(96, 334);
+            this.labelStad.Name = "labelStad";
+            this.labelStad.Size = new System.Drawing.Size(37, 17);
+            this.labelStad.TabIndex = 8;
+            this.labelStad.Text = "Stad";
+            // 
+            // labelLand
+            // 
+            this.labelLand.AutoSize = true;
+            this.labelLand.Location = new System.Drawing.Point(96, 367);
+            this.labelLand.Name = "labelLand";
+            this.labelLand.Size = new System.Drawing.Size(40, 17);
+            this.labelLand.TabIndex = 9;
+            this.labelLand.Text = "Land";
+            // 
+            // labelTelefoonnummer
+            // 
+            this.labelTelefoonnummer.AutoSize = true;
+            this.labelTelefoonnummer.Location = new System.Drawing.Point(96, 400);
+            this.labelTelefoonnummer.Name = "labelTelefoonnummer";
+            this.labelTelefoonnummer.Size = new System.Drawing.Size(115, 17);
+            this.labelTelefoonnummer.TabIndex = 10;
+            this.labelTelefoonnummer.Text = "Telefoonnummer";
+            // 
+            // labelTelefoonnummerType
+            // 
+            this.labelTelefoonnummerType.AutoSize = true;
+            this.labelTelefoonnummerType.Location = new System.Drawing.Point(96, 433);
+            this.labelTelefoonnummerType.Name = "labelTelefoonnummerType";
+            this.labelTelefoonnummerType.Size = new System.Drawing.Size(146, 17);
+            this.labelTelefoonnummerType.TabIndex = 11;
+            this.labelTelefoonnummerType.Text = "Telefoonnummer type";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(96, 466);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(47, 17);
+            this.labelEmail.TabIndex = 12;
+            this.labelEmail.Text = "E-mail";
+            // 
+            // textBoxTelType
+            // 
+            this.textBoxTelType.Location = new System.Drawing.Point(284, 430);
+            this.textBoxTelType.Name = "textBoxTelType";
+            this.textBoxTelType.Size = new System.Drawing.Size(156, 22);
+            this.textBoxTelType.TabIndex = 13;
+            // 
+            // textBoxTel
+            // 
+            this.textBoxTel.Location = new System.Drawing.Point(284, 397);
+            this.textBoxTel.Name = "textBoxTel";
+            this.textBoxTel.Size = new System.Drawing.Size(156, 22);
+            this.textBoxTel.TabIndex = 14;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(284, 463);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(156, 22);
+            this.textBoxEmail.TabIndex = 15;
+            // 
+            // textBoxBSN
+            // 
+            this.textBoxBSN.Location = new System.Drawing.Point(284, 100);
+            this.textBoxBSN.Name = "textBoxBSN";
+            this.textBoxBSN.Size = new System.Drawing.Size(156, 22);
+            this.textBoxBSN.TabIndex = 16;
+            // 
+            // textBoxVoornaam
+            // 
+            this.textBoxVoornaam.Location = new System.Drawing.Point(284, 133);
+            this.textBoxVoornaam.Name = "textBoxVoornaam";
+            this.textBoxVoornaam.Size = new System.Drawing.Size(156, 22);
+            this.textBoxVoornaam.TabIndex = 17;
+            // 
+            // textBoxAchternaam
+            // 
+            this.textBoxAchternaam.Location = new System.Drawing.Point(284, 166);
+            this.textBoxAchternaam.Name = "textBoxAchternaam";
+            this.textBoxAchternaam.Size = new System.Drawing.Size(156, 22);
+            this.textBoxAchternaam.TabIndex = 18;
+            // 
+            // comboBoxGeslacht
+            // 
+            this.comboBoxGeslacht.FormattingEnabled = true;
+            this.comboBoxGeslacht.Location = new System.Drawing.Point(284, 199);
+            this.comboBoxGeslacht.Name = "comboBoxGeslacht";
+            this.comboBoxGeslacht.Size = new System.Drawing.Size(156, 24);
+            this.comboBoxGeslacht.TabIndex = 19;
+            // 
+            // textBoxAdres
+            // 
+            this.textBoxAdres.Location = new System.Drawing.Point(284, 232);
+            this.textBoxAdres.Name = "textBoxAdres";
+            this.textBoxAdres.Size = new System.Drawing.Size(156, 22);
+            this.textBoxAdres.TabIndex = 20;
+            // 
+            // textBoxHuisnummer
+            // 
+            this.textBoxHuisnummer.Location = new System.Drawing.Point(284, 265);
+            this.textBoxHuisnummer.Name = "textBoxHuisnummer";
+            this.textBoxHuisnummer.Size = new System.Drawing.Size(156, 22);
+            this.textBoxHuisnummer.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(284, 298);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
+            this.textBox1.TabIndex = 22;
+            // 
+            // textBoxStad
+            // 
+            this.textBoxStad.Location = new System.Drawing.Point(284, 331);
+            this.textBoxStad.Name = "textBoxStad";
+            this.textBoxStad.Size = new System.Drawing.Size(156, 22);
+            this.textBoxStad.TabIndex = 23;
+            // 
+            // textBoxLand
+            // 
+            this.textBoxLand.Location = new System.Drawing.Point(284, 364);
+            this.textBoxLand.Name = "textBoxLand";
+            this.textBoxLand.Size = new System.Drawing.Size(156, 22);
+            this.textBoxLand.TabIndex = 24;
+            // 
+            // labelGebruikersnaam
+            // 
+            this.labelGebruikersnaam.AutoSize = true;
+            this.labelGebruikersnaam.Location = new System.Drawing.Point(576, 103);
+            this.labelGebruikersnaam.Name = "labelGebruikersnaam";
+            this.labelGebruikersnaam.Size = new System.Drawing.Size(113, 17);
+            this.labelGebruikersnaam.TabIndex = 25;
+            this.labelGebruikersnaam.Text = "Gebruikersnaam";
+            // 
+            // labelWachtwoord
+            // 
+            this.labelWachtwoord.AutoSize = true;
+            this.labelWachtwoord.Location = new System.Drawing.Point(576, 136);
+            this.labelWachtwoord.Name = "labelWachtwoord";
+            this.labelWachtwoord.Size = new System.Drawing.Size(86, 17);
+            this.labelWachtwoord.TabIndex = 26;
+            this.labelWachtwoord.Text = "Wachtwoord";
+            // 
+            // labelHerhaalWachtwoord
+            // 
+            this.labelHerhaalWachtwoord.AutoSize = true;
+            this.labelHerhaalWachtwoord.Location = new System.Drawing.Point(576, 169);
+            this.labelHerhaalWachtwoord.Name = "labelHerhaalWachtwoord";
+            this.labelHerhaalWachtwoord.Size = new System.Drawing.Size(140, 17);
+            this.labelHerhaalWachtwoord.TabIndex = 27;
+            this.labelHerhaalWachtwoord.Text = "Herhaal Wachtwoord";
+            // 
+            // textBoxGebruikersnaam
+            // 
+            this.textBoxGebruikersnaam.Location = new System.Drawing.Point(752, 100);
+            this.textBoxGebruikersnaam.Name = "textBoxGebruikersnaam";
+            this.textBoxGebruikersnaam.Size = new System.Drawing.Size(156, 22);
+            this.textBoxGebruikersnaam.TabIndex = 28;
+            // 
+            // textBoxWachtwoord
+            // 
+            this.textBoxWachtwoord.Location = new System.Drawing.Point(752, 133);
+            this.textBoxWachtwoord.Name = "textBoxWachtwoord";
+            this.textBoxWachtwoord.Size = new System.Drawing.Size(156, 22);
+            this.textBoxWachtwoord.TabIndex = 29;
+            // 
+            // textBoxHerhaalWachtwoord
+            // 
+            this.textBoxHerhaalWachtwoord.Location = new System.Drawing.Point(752, 166);
+            this.textBoxHerhaalWachtwoord.Name = "textBoxHerhaalWachtwoord";
+            this.textBoxHerhaalWachtwoord.Size = new System.Drawing.Size(156, 22);
+            this.textBoxHerhaalWachtwoord.TabIndex = 30;
+            // 
+            // buttonBevestigen
+            // 
+            this.buttonBevestigen.Location = new System.Drawing.Point(957, 466);
+            this.buttonBevestigen.Name = "buttonBevestigen";
+            this.buttonBevestigen.Size = new System.Drawing.Size(125, 43);
+            this.buttonBevestigen.TabIndex = 31;
+            this.buttonBevestigen.Text = "Bevestigen";
+            this.buttonBevestigen.UseVisualStyleBackColor = true;
+            // 
+            // CreateUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 531);
+            this.Controls.Add(this.buttonBevestigen);
+            this.Controls.Add(this.textBoxHerhaalWachtwoord);
+            this.Controls.Add(this.textBoxWachtwoord);
+            this.Controls.Add(this.textBoxGebruikersnaam);
+            this.Controls.Add(this.labelHerhaalWachtwoord);
+            this.Controls.Add(this.labelWachtwoord);
+            this.Controls.Add(this.labelGebruikersnaam);
+            this.Controls.Add(this.textBoxLand);
+            this.Controls.Add(this.textBoxStad);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxHuisnummer);
+            this.Controls.Add(this.textBoxAdres);
+            this.Controls.Add(this.comboBoxGeslacht);
+            this.Controls.Add(this.textBoxAchternaam);
+            this.Controls.Add(this.textBoxVoornaam);
+            this.Controls.Add(this.textBoxBSN);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxTel);
+            this.Controls.Add(this.textBoxTelType);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelTelefoonnummerType);
+            this.Controls.Add(this.labelTelefoonnummer);
+            this.Controls.Add(this.labelLand);
+            this.Controls.Add(this.labelStad);
+            this.Controls.Add(this.labelPostcode);
+            this.Controls.Add(this.labelHuisnummer);
+            this.Controls.Add(this.labelAdres);
+            this.Controls.Add(this.labelGeslacht);
+            this.Controls.Add(this.labelAchternaam);
+            this.Controls.Add(this.labelVoornaam);
+            this.Controls.Add(this.labelBSN);
+            this.Controls.Add(this.labelAccountAanmaken);
+            this.Name = "CreateUser";
             this.Text = "CreateUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAccountAanmaken;
+        private System.Windows.Forms.Label labelBSN;
+        private System.Windows.Forms.Label labelVoornaam;
+        private System.Windows.Forms.Label labelAchternaam;
+        private System.Windows.Forms.Label labelGeslacht;
+        private System.Windows.Forms.Label labelAdres;
+        private System.Windows.Forms.Label labelHuisnummer;
+        private System.Windows.Forms.Label labelPostcode;
+        private System.Windows.Forms.Label labelStad;
+        private System.Windows.Forms.Label labelLand;
+        private System.Windows.Forms.Label labelTelefoonnummer;
+        private System.Windows.Forms.Label labelTelefoonnummerType;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxTelType;
+        private System.Windows.Forms.TextBox textBoxTel;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxBSN;
+        private System.Windows.Forms.TextBox textBoxVoornaam;
+        private System.Windows.Forms.TextBox textBoxAchternaam;
+        private System.Windows.Forms.ComboBox comboBoxGeslacht;
+        private System.Windows.Forms.TextBox textBoxAdres;
+        private System.Windows.Forms.TextBox textBoxHuisnummer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxStad;
+        private System.Windows.Forms.TextBox textBoxLand;
+        private System.Windows.Forms.Label labelGebruikersnaam;
+        private System.Windows.Forms.Label labelWachtwoord;
+        private System.Windows.Forms.Label labelHerhaalWachtwoord;
+        private System.Windows.Forms.TextBox textBoxGebruikersnaam;
+        private System.Windows.Forms.TextBox textBoxWachtwoord;
+        private System.Windows.Forms.TextBox textBoxHerhaalWachtwoord;
+        private System.Windows.Forms.Button buttonBevestigen;
     }
 }

f523eb936ce46b0b73308cc7511bd3d29ecb6319
MitchBrinkman21 <Mitch.Brinkman19@icloud.com>
2014-12-05 01:58:12 -0800

    TransactionServer added to project.

---
+++ TransactionServer/TransactionServer/FormMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransactionServer
+{
+    public partial class FormMain : Form
+    {
+        public static SqlConnection conn;
+        ExecuteThread executeThread;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            conn = DatabaseConnection.CONN;
+
+            buttonStop.Enabled = false;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            executeThread = new ExecuteThread();
+            buttonStop.Enabled = true;
+            buttonStart.Enabled = false;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            executeThread.keepRunning = false;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void timerInvalidateForm_Tick(object sender, EventArgs e)
+        {
+            textBoxTransactions.Text = TransactionTask.succesful.ToString();
+            textBoxErrors.Text = TransactionTask.errors.ToString();
+            textBoxChecksum.Text = ExecuteThread.checksum.ToString();
+            this.Refresh();
+        }
+    }
+}
---
+++ TransactionServer/TransactionServer/Transaction.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionServer
+{
+    class Transaction
+    {
+        private int transactionId;
+        private int transactionStatusId;
+        private int sepa; 
+        private String ibanReceiver;
+        private double amount;
+        private String iban;
+        private double balance;
+
+        public void SetTransactionId(int transactionId)
+        {
+            this.transactionId = transactionId;
+        }
+
+        public int GetTransactionId()
+        {
+            return this.transactionId;
+        }
+
+        public void SetTransactionStatusId(int transactionStatusId)
+        {
+            this.transactionStatusId = transactionStatusId;
+        }
+
+        public int GetTransactionStatusId()
+        {
+            return this.transactionStatusId;
+        }
+
+        public void SetSepa(int sepa)
+        {
+            this.sepa = sepa;
+        }
+
+        public int GetSepa()
+        {
+            return this.sepa;
+        }
+
+        public void SetIbanReceiver(String ibanReceiver)
+        {
+            this.ibanReceiver = ibanReceiver;
+        }
+
+        public String GetIbanReceiver()
+        {
+            return this.ibanReceiver;
+        }
+
+        public void SetAmount(double amount)
+        {
+            this.amount = amount;
+        }
+
+        public double GetAmount()
+        {
+            return this.amount;
+        }
+
+        public void SetIban(String iban)
+        {
+            this.iban = iban;
+        }
+
+        public String GetIban()
+        {
+            return this.iban;
+        }
+
+        public void SetBalance(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double GetBalance()
+        {
+            return this.balance;
+        }
+    }
+}

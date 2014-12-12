a03088acc280001a263fad703eb7da7be32726e6
Joey Klaassen <joeyklaassen@gmail.com>
2014-12-12 14:24:46 +0100

    - Updated App.Config - Updated UnitTestCreateUser.cs

--- Q-BankTests/UnitTestCreateUser.cs
+++ Q-BankTests/UnitTestCreateUser.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Q_Bank.Controller;
 using Q_Bank.View;
+using Q_Bank.Pro6PPServiceReference;
 using System.Data.SqlClient;
 
 namespace Q_BankTests
@@ -34,15 +35,177 @@ namespace Q_BankTests
         }
 
         [TestMethod]
+        public void TestWrongFirstname()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxFirstname.Text = "123";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(expected, actual, "Wrong Firstname is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongLastname()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxLastname.Text = "123";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(expected, actual, "Wrong Lastname is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongBirthday()
+        {
+            CreateUser cu = new CreateUser();
+            cu.dateTimePickerBirthday.Value = Convert.ToDateTime("2014-12-13");
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(expected, actual, "Wrong Birthday is not detected");
+        }
+
+        [TestMethod]
         public void TestWrongZipcode()
         {
             CreateUser cu = new CreateUser();
             cu.textBoxZipcode.Text = "386PN";
             CreateUserController cuc = new CreateUserController(cu);
-            bool actual = cuc.checkValidZipcode(cu.textBoxZipcode.Text);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
             bool expected = false;
 
             Assert.AreEqual<bool>(actual, expected, "Wrong Zipcode is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongNumber()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxNumber.Text = "A32";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Wrong Number is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongAddress()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxAddress.Text = "123";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Wrong Address is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongCity()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxCity.Text = "123";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Wrong City is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongCountry()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxCountry.Text = "123";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Wrong Country is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongPhonenumber()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxPhonenumber.Text = "abc";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Wrong Phonenumber is not detected");
+        }
+
+        [TestMethod]
+        public void TestWrongEmail()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxEmail.Text = "123abc";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkValidText(new object(), null);
+            bool actual = cuc.validText;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Wrong Email is not detected");
+        }
+
+        [TestMethod]
+        public void TestExistingUsername()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxUsername.Text = "Piet";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkUsername(new object(), null);
+            bool actual = cuc.validUsername;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Existing Username is not detected");
+        }
+
+        [TestMethod]
+        public void TestUnequalPassword()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxPassword.Text = "abc";
+            cu.textBoxRepeatPassword.Text = "123";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.checkPassword(new object(), null);
+            bool actual = cuc.validPassword;
+            bool expected = false;
+
+            Assert.AreEqual<bool>(actual, expected, "Unequal password is not detected");
+        }
+
+        [TestMethod]
+        public void TestFillAddress()
+        {
+            CreateUser cu = new CreateUser();
+            cu.textBoxZipcode.Text = "3864PN";
+            cu.textBoxNumber.Text = "25b";
+            CreateUserController cuc = new CreateUserController(cu);
+            cuc.fillAddress(new object(), null);
+            string actualAddress = cu.textBoxAddress.Text;
+            string actualCity = cu.textBoxCity.Text;
+            string expectedAddress = "Domstraat";
+            string expectedCity = "Nijkerkerveen";
+
+            Assert.AreEqual<string>(actualAddress, expectedAddress, "Wrong address");
+            Assert.AreEqual<string>(actualCity, expectedCity, "Wrong city");
         }
     }
 }

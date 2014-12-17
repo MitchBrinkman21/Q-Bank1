ee1dd5cabd72ef404f073ff7b8838a60794e3537
pokke123 <adriaankoman@gmail.com>
2014-12-17 11:56:08 +0100

    no message

---
+++ Q-Bank-Administration/Q-Bank-Administration/Controller/CostumerOverviewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_Bank_Administration.Controller
+{
+    class CostumerOverviewController
+    {
+        public FormMain formMain { get; set; }
+
+        public CostumerOverviewController(FormMain formMain)
+        {
+            this.formMain = formMain;
+        }
+
+        private void tabControl(object sender, EventArgs e)
+        {
+        }
+    }
+}
